d7b1cedb57950f54c9f33e9881fd4717fd91db97
nyinyi979 <nyinyi095062687@gmail.com>
2023-11-08 18:19:49 +0630

    token

--- app/_componenets/dynamic_components/getImageData.tsx
+++ app/_componenets/dynamic_components/getImageData.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Dropbox } from "dropbox";
-let accessToken = 'sl.BpfuNSx-z9puFtYK33cjsS6ti2HFpaqPiOxgFQOvj9iEsL45b_AYrcTIt3OzmW39H7UmvtbqwdaM18lOrigXdWfwXZN6tPpxMmZFcSUGY3WBQsOaoYfQoUj4o9mNyg-UiAguL_yW_5CoEzANSTUsBFU';
+let accessToken = 'sl.BpcjqqsV27ffpOWfW6GcDpF_cbpB9M1Kc8yE6tOT-6jz1Ooqesg2LhIcl8c_0yndJ-CIvHYBIvWORkwfYit1eAyfuWq902bLYI3eflcs-QZTlOESCu9FIghlJ5EJKaS1jMcdK1V75JOEbBbeHutFkaE';
 setInterval(()=>{
     axios({
         url: process.env.NEXT_PUBLIC_BASE_FETCH_URL + '/getToken'
